5e27f6be6bb4ae3fbde9dd2cb7dfeb05a5632aed
Simon <slam74@myseneca.ca>
2021-08-28 02:39:47 -0400

    footer changes

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,7 +9,7 @@ function CardItem(props) {
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
-              alt='Project Image'
+              alt='Project Pikachu'
               src={props.src}
             />
           </figure>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
                 <h2>Thanks for Visiting!</h2>
                 <div className='links'>
                     <a href="mailto:simon.lam06@gmail.com"><i class="fas fa-envelope"></i></a>
-                    <a href="https://www.linkedin.com/in/simon-lam247/" target="_blank"><i class="fab fa-linkedin"></i></a>
-                    <a href="https://github.com/slam74" target="_blank"><i class="fab fa-github-square"></i></a>
+                    <a href="https://www.linkedin.com/in/simon-lam247/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://github.com/slam74" target="_blank" rel="noreferrer"><i class="fab fa-github-square"></i></a>
                 </div>
                 <h2>Simon Lam © 2021</h2>
             </section>

7f390e8260155f17c30ef7a0074944ad35adf00b
Simon <slam74@myseneca.ca>
2021-08-30 21:05:36 -0400

    card edit

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,18 +5,18 @@ function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
+        <a href={props.link} target="_blank" rel="noopener noreferrer" className='cards__item__link'>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
-              alt='Project Pikachu'
+              alt=''
               src={props.src}
             />
           </figure>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
           </div>
-        </Link>
+        </a>
       </li>
     </>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Here is some of my work</h1>
+      <h1>Here is some of my work on Github</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
@@ -13,13 +13,13 @@ function Cards() {
               src='./images/airbnb-1.jpg'
               text='Airbnb Website Emulation'
               label='JavaScript, NodeJS, Express, Handlebars, HTML, CSS, Postgresql'
-              path='/'
+              link='https://github.com/slam74/Airbnb-Website-Emulation/'
             />
             <CardItem
               src='./images/assembly-line.jpg'
               text='Assembly Line Simulation'
               label='C++'
-              path='/'
+              link='https://github.com/slam74/Assembly-Line-Simulation'
             />
           </ul>
           <ul className='cards__items'>
@@ -27,13 +27,13 @@ function Cards() {
               src='./images/retail-app.jpg'
               text='Retail Application'
               label='C++, PL/SQL'
-              path='/'
+              link='https://github.com/slam74/DBS311-Advanced-Data-Services'
             />
             <CardItem
               src='./images/hr-app.jpg'
               text='HR Application'
               label='C++, SQL'
-              path='/'
+              link='https://github.com/slam74/HR-Application'
             />
           </ul>
           <ul className='cards__items'>
@@ -41,13 +41,13 @@ function Cards() {
               src='./images/valet-parking.jpg'
               text='Valet Parking Application'
               label='C++'
-              path='/'
+              link='https://github.com/slam74/Valet-Parking-Application'
             />
             <CardItem
               src='./images/badminton-shop.jpg'
               text='Badminton Equipment Store Website'
               label='JavaScript, HTML5, CSS3'
-              path='/'
+              link='https://github.com/slam74/Badminton-Equipment-Store-Website'
             />
           </ul>
           <ul className='cards__items'>
@@ -55,13 +55,13 @@ function Cards() {
               src='./images/contact-management-app.jpg'
               text='Contact Management System Application'
               label='C'
-              path='/'
+              link='https://github.com/slam74/Contact-Management-System-Application'
             />
             <CardItem
               src='./images/api.jpg'
               text='Accessing Restaurant Data with an API'
               label='JavaScript, Lodash, Moment.js, Leaflet, jQuery, Bootstrap'
-              path='/'
+              link='https://github.com/slam74/Accessing-Restaurant-Data-with-an-API'
             />
           </ul>
         </div>
